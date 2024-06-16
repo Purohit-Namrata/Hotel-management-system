6393c39f3a30980e57473373801202d2b712a130
Purohit-Namrata <namratapurohit78@gmail.com>
2024-06-16 23:10:29 +0530

    Update Customerlist.py

--- Customerlist.py
+++ Customerlist.py
@@ -2,29 +2,18 @@ from tkinter import *
 from tkinter import messagebox
 import pymysql
 
-#Conncect to database
-#ENTER YOUR USER NAME AND PASSWORD TO CONNECT TO DATABASE
 con = pymysql.connect(host="localhost",user="root",password="ROOT123",database="Hotel_man")
 cur = con.cursor()
 
-#Table connection
-custTable="Cust" #Customer Table
+custTable="Cust" 
 def View(): 
-    
     root = Tk()
     root.title("Customer List")
     root.minsize(width=400,height=400)
     root.geometry("600x500")
 
-    Canvas1 = Canvas(root) 
-    Canvas1.config(bg="#12a4d9")
-    Canvas1.pack(expand=True,fill=BOTH)
-        
-    
-    headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
-    headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
-        
-    headingLabel = Label(headingFrame1, text="View Customer List", bg='black', fg='white', font=('Courier',15))
+
+    headingLabel = Label(root, text="View Customer List", bg='black', fg='white', font=('Courier',15))
     headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
     
     labelFrame = Frame(root,bg='black')
