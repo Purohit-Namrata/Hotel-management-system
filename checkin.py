987728fe7602608c7e6b2d405a9c70d988781658
Purohit-Namrata <namratapurohit78@gmail.com>
2024-06-16 23:51:38 +0530

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -80,14 +80,8 @@ def checkin():
     root.minsize(width=400,height=400)
     root.geometry("600x500")
     
-    Canvas1 = Canvas(root)
-    Canvas1.config(bg="#D6ED17")
-    Canvas1.pack(expand=True,fill=BOTH)
-
-    headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
-    headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
         
-    headingLabel = Label(headingFrame1, text="Issue Room", bg='black', fg='white', font=('Courier',15))
+    headingLabel = Label(root, text="Issue Room", bg='black', fg='white', font=('Courier',15))
     headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
     
     labelFrame = Frame(root,bg='black')
