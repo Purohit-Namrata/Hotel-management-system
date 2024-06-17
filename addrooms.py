1c0accc5f46c918523461822df7a515a519e3915
Purohit-Namrata <namratapurohit78@gmail.com>
2024-06-17 07:45:03 +0530

    Update addrooms.py

--- addrooms.py
+++ addrooms.py
@@ -7,25 +7,19 @@ def addroomss():
     RoomNO = custInfo1.get()
     RoomType = custInfo2.get()
     Status = custInfo3.get()
-    
-    
-    
+     
     insertCust = "insert into "+roomTable+" values('"+RoomNO+"','"+RoomType+"','"+Status+"')"
     try:
         cur.execute(insertCust)
         con.commit()
         messagebox.showinfo('Sucess',"Room Added successfully")
     except:
-        messagebox.showinfo("Error","Room Already Exists")
-        
-    print(RoomNO)
-    print(RoomType)
-    print(Status)
-
-
-
+        messagebox.showinfo("Error","Room Already Exists")    
 
     root.destroy()
+
+
+
 def addrooms():
     global custInfo1, custInfo2, custInfo3, custInfo4, Canvas1, con, cur, roomTable,root
 
@@ -34,17 +28,13 @@ def addrooms():
     root.minsize(width=400,height=400)
     root.geometry("600x500")
 
-
-   
     con = pymysql.connect(host="localhost",user="root",password="ROOT123",database="Hotel_man")
     cur = con.cursor()
 
     roomTable= "rooms"     
 
-
     headingLabel = Label(root, text="Add Rooms", bg='black', fg='white', font=('Courier',15))
     headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
-
 
     labelFrame = Frame(root,bg='black')
     labelFrame.place(relx=0.1,rely=0.4,relwidth=0.8,relheight=0.4)
@@ -55,7 +45,6 @@ def addrooms():
     custInfo1 = Entry(labelFrame)
     custInfo1.place(relx=0.3,rely=0.2, relwidth=0.62, relheight=0.08)
     
-
     lb2 = Label(labelFrame,text="Room Type : ", bg='black', fg='white')
     lb2.place(relx=0.05,rely=0.35, relheight=0.08)
         
@@ -67,12 +56,11 @@ def addrooms():
     
     custInfo3 = Entry(labelFrame)
     custInfo3.place(relx=0.3,rely=0.50, relwidth=0.62, relheight=0.08)
-
    
-    SubmitBtn = Button(root,text="Add Room",bg='#d1ccc0', fg='black',command=addroomss)
+    SubmitBtn = Button(root,text="Add Room",bg='Orange', fg='black',command=addroomss)
     SubmitBtn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
     
-    quitBtn = Button(root,text="Quit",bg='#f7f1e3', fg='black', command=root.destroy)
+    quitBtn = Button(root,text="Quit",bg='Yellow', fg='black', command=root.destroy)
     quitBtn.place(relx=0.53,rely=0.9, relwidth=0.18,relheight=0.08)
     
     root.mainloop()
